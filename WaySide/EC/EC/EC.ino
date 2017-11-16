2b65a1f4ebe2dac2f5bd3a0c9d0d9f4f0b51a19d
Jan Bertelsen <jb-git@w57.dk>
2017-11-17 00:50:26 +0100

    Updated EC code

--- WaySide/EC/EC/EC.ino
+++ WaySide/EC/EC/EC.ino
@@ -7,8 +7,8 @@
 // -------------------------------------------------------------------------------------- Type
 struct elementType {
   byte type;
-  byte device1;
-  byte device2;
+  byte deviceMajor;
+  byte deviceMinor;
   byte order;
   byte state;
   byte cStatus;
@@ -18,12 +18,12 @@ struct elementType {
 // -------------------------------------------------------------------------------------- Ennumeration
 
 // Orders
-#define S_STOP 31 // Signal
+#define S_STOP 31     // Signal
 #define S_PROCEED 32
 #define S_PROCEEDPROCEED 33
-#define R_PASS 41 // Road signal
+#define R_PASS 41     // Road signal
 #define R_STOP 42
-#define P_RIGHT 11
+#define P_RIGHT 11    // Point machine
 #define P_LEFT 12
 #define P_RIGHT_HOLD 13
 #define P_LEFT_HOLD 14
@@ -150,22 +150,22 @@ void checkTimer() {
       switch (element[index].type) {
         case 30: // Road signal
           if (element[index].order == R_STOP) {
-            Ldevice(element[index].device1, flash);
+            Ldevice(element[index].deviceMajor, flash);
           }
           break;
         case 31: // Road signal
           if (element[index].order == R_STOP) {
-            Udevice(element[index].device1, flash);
+            Udevice(element[index].deviceMajor, flash);
           }
           break;
-        case 42: // Signal, three aspect, L-devices
+        case 42: // Signal, 3 aspect, 2 L-devices
           if (element[index].order == S_PROCEEDPROCEED) {
-            Ldevice(element[index].device2, flash);
+            Ldevice(element[index].deviceMajor + 1, flash);
           }
           break;
-        case 43: // Signal, three aspect, U-devices
+        case 43: // Signal, 3 aspect, 2 U-devices
           if (element[index].order == S_PROCEEDPROCEED) {
-            Udevice(element[index].device2, flash);
+            Udevice(element[index].deviceMajor + 1, flash);
           }
           break;
       }
@@ -179,26 +179,26 @@ void checkTimer() {
       switch (element[index].type) {
         case 10: // point machine w/o position detector
           switch (element[index].state) {
-            case PM_POL_RIGHT:
-              Pdevice(element[index].device1, HIGH, HIGH);
+            case PM_POL_RIGHT: // activate polarity relay shortly before throw relay
+              Pdevice(element[index].deviceMajor, HIGH, HIGH);
               element[index].state = PM_THROW_RIGHT;
               element[index].timer = (element[index].order == P_RIGHT_HOLD ? PM_THROW_HOLD_TIME : PM_THROW_TIME);
               element[index].cStatus = (element[index].order == P_RIGHT_HOLD ? I_U_RIGHT_HOLDING : I_U_RIGHT);
               break;
             case PM_THROW_RIGHT:
-              Pdevice(element[index].device1, HIGH, LOW);
+              Pdevice(element[index].deviceMajor, HIGH, LOW);
               element[index].state = PM_POL_RIGHT_DONE;
               element[index].timer = PM_POL_TIME;
               //              element[index].cStatus = ;
               break;
             case PM_POL_RIGHT_DONE:
-              Pdevice(element[index].device1, LOW, LOW);
+              Pdevice(element[index].deviceMajor, LOW, LOW);
               element[index].state = PM_IDLE;
               element[index].timer = 0;
               //              element[index].cState = ;
               break;
             case PM_THROW_LEFT:
-              Pdevice(element[index].device1, LOW, LOW);
+              Pdevice(element[index].deviceMajor, LOW, LOW);
               element[index].state = PM_IDLE;
               element[index].timer = 0;
               element[index].cStatus = (element[index].order == P_LEFT_HOLD ? I_U_LEFT_HOLDING : I_U_LEFT);
@@ -206,36 +206,50 @@ void checkTimer() {
           }
           break;
         case 21: // semaphore
-          Pdevice(element[index].device1, LOW, LOW);
+          Pdevice(element[index].deviceMajor, LOW, LOW);
           element[index].order = S_STOP;
           element[index].cStatus = I_STOP;
           break;
-        case 40: // signal 2 L-device
-        case 42: // signal 3 L-device
-          Ldevice(element[index].device1, HIGH);
-          Ldevice(element[index].device2, LOW);
+        case 40: // signal 2 aspect, 2 L-device
+        case 42: // signal 3 aspect, 2 L-device
+          Ldevice(element[index].deviceMajor, HIGH);
+          Ldevice(element[index].deviceMajor + 1, LOW);
           element[index].order = S_STOP;
           element[index].cStatus = I_STOP;
           break;
-        case 41: // signal 2 U-device
-          Udevice(element[index].device1, LOW);
+        case 44: // signal 3 aspect, 3 L-device
+          Ldevice(element[index].deviceMajor, HIGH);
+          Ldevice(element[index].deviceMajor + 1, LOW);
+          Ldevice(element[index].deviceMajor + 2, LOW);
           element[index].order = S_STOP;
           element[index].cStatus = I_STOP;
           break;
-        case 43: // signal 3 U-device
-          Udevice(element[index].device1, HIGH);
-          Udevice(element[index].device2, LOW);
+        case 41: // signal 1 U-device
+          Udevice(element[index].deviceMajor, LOW);
+          element[index].order = S_STOP;
+          element[index].cStatus = I_STOP;
+          break;
+        case 43: // signal 3 aspect, 2 U-device
+          Udevice(element[index].deviceMajor, HIGH);
+          Udevice(element[index].deviceMajor + 1, LOW);
+          element[index].order = S_STOP;
+          element[index].cStatus = I_STOP;
+          break;
+        case 45: // signal 3 aspect, 3 U-device
+          Udevice(element[index].deviceMajor, HIGH);
+          Udevice(element[index].deviceMajor + 1, LOW);
+          Udevice(element[index].deviceMajor + 2, LOW);
           element[index].order = S_STOP;
           element[index].cStatus = I_STOP;
           break;
         case 32: // road barrier
           switch (element[index].cStatus) {
             case I_MOVING_DOWN:
-              Pdevice(element[index].device1, LOW, LOW);
+              Pdevice(element[index].deviceMajor, LOW, LOW);
               element[index].cStatus = I_CLOSED;
               break;
             case I_MOVING_UP:
-              Pdevice(element[index].device1, LOW, LOW);
+              Pdevice(element[index].deviceMajor, LOW, LOW);
               element[index].cStatus = I_OPEN;
               break;
           }
@@ -336,36 +350,82 @@ boolean AbusRecThis() {
       break;
     case 20: // Type 20, Configuration data
       switch (Abus.fromM[3]) { // Configuration command
-        case 0: // clear configuration
+        case 0: // -------------------------------------------------------------- Clear configuration
           nextElement = 0;
           Abus.toM[3] = 0;
           break;
-        case 1: // Add configuration
+        case 1: // -------------------------------------------------------------- Add configuration
           if (nextElement < N_ELEMENT ) {
             switch (Abus.fromM[4]) { // element type
+              // ------------------------------------------------------------------------- U-devices
               case 31: // Level crossing road signal
-              case 41: // Light signal, two aspect
+              case 41: // Light signal, 2 aspect
                 if (Abus.fromM[5] > 0 and Abus.fromM[5] <= N_UDEVICE) {
                   element[nextElement].type = Abus.fromM[4];
-                  element[nextElement].device1 = Abus.fromM[5];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
                   pinMode(UdevicePin[Abus.fromM[5] - 1], OUTPUT);
                   digitalWrite(UdevicePin[Abus.fromM[5] - 1], LOW);
                 } else {
                   Abus.toM[3] = 1; // invalid device number 1
                 }
                 break;
-              case 32: // Road barrier
-                if (Abus.fromM[5] > 0 and Abus.fromM[5] <= N_PDEVICE) {
+              case 43: // Light signal, 2 lantern, 3 aspect
+                if (Abus.fromM[5] > 0 and Abus.fromM[5] + 1 <= N_UDEVICE) {
                   element[nextElement].type = Abus.fromM[4];
-                  element[nextElement].device1 = Abus.fromM[5];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
+                  pinMode(UdevicePin[Abus.fromM[5] - 1], OUTPUT);
+                  digitalWrite(UdevicePin[Abus.fromM[5] - 1], LOW);
+                  pinMode(UdevicePin[Abus.fromM[5]], OUTPUT);
+                  digitalWrite(UdevicePin[Abus.fromM[5]], LOW);
                 } else {
                   Abus.toM[3] = 1; // invalid device number 1
                 }
                 break;
-              case 10: // Point 
+              case 45: // Light signal, 3 lantern, 3 aspecs
+                if (Abus.fromM[5] > 0 and Abus.fromM[5] + 2 <= N_UDEVICE) {
+                  element[nextElement].type = Abus.fromM[4];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
+                  pinMode(UdevicePin[Abus.fromM[5] - 1], OUTPUT);
+                  digitalWrite(UdevicePin[Abus.fromM[5] - 1], LOW);
+                  pinMode(UdevicePin[Abus.fromM[5]], OUTPUT);
+                  digitalWrite(UdevicePin[Abus.fromM[5]], LOW);
+                  pinMode(UdevicePin[Abus.fromM[5] + 1], OUTPUT);
+                  digitalWrite(UdevicePin[Abus.fromM[5] + 1], LOW);
+                } else {
+                  Abus.toM[3] = 1; // invalid device number 1
+                }
+                break;
+              // ------------------------------------------------------------------------------------P-devices
+              case 32: // Road barrier
                 if (Abus.fromM[5] > 0 and Abus.fromM[5] <= N_PDEVICE) {
                   element[nextElement].type = Abus.fromM[4];
-                  element[nextElement].device1 = Abus.fromM[5];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
+                } else {
+                  Abus.toM[3] = 1; // invalid device number 1
+                }
+                break;
+              case 10: // Point without feedback, P-device
+                if (Abus.fromM[5] > 0 and Abus.fromM[5] <= N_PDEVICE) {
+                  element[nextElement].type = Abus.fromM[4];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
+                } else {
+                  Abus.toM[3] = 1; // invalid device number 1
+                }
+                break;
+              // -------------------------------------------------------------------------------- L-device
+              case 40: // Signal, 2 lantern, 2 aspects
+              case 42: // Signal, 2 lantern, 3 aspects
+                if (Abus.fromM[5] > 0 and Abus.fromM[5] + 1 <= N_PDEVICE) {
+                  element[nextElement].type = Abus.fromM[4];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
+                } else {
+                  Abus.toM[3] = 1; // invalid device number 1
+                }
+                break;
+              case 44: // Signal, 3 lantern, 3 aspects, L-device
+                if (Abus.fromM[5] > 0 and Abus.fromM[5] + 2 <= N_PDEVICE) {
+                  element[nextElement].type = Abus.fromM[4];
+                  element[nextElement].deviceMajor = Abus.fromM[5];
                 } else {
                   Abus.toM[3] = 1; // invalid device number 1
                 }
@@ -397,82 +457,130 @@ void elementOrder(byte index, byte order) {
         switch (order) {
           case P_RIGHT:
           case P_RIGHT_HOLD:
-            Pdevice(element[index].device1, HIGH, LOW);
+            Pdevice(element[index].deviceMajor, HIGH, LOW);
             element[index].state = PM_POL_RIGHT;
             element[index].timer = PM_POL_TIME;
             element[index].cStatus = UNSUPERVISED;
             break;
           case P_LEFT:
           case P_LEFT_HOLD:
-            Pdevice(element[index].device1, LOW, HIGH);
+            Pdevice(element[index].deviceMajor, LOW, HIGH);
             element[index].state = PM_THROW_LEFT;
             element[index].timer = (order == P_LEFT_HOLD ? PM_THROW_HOLD_TIME : PM_THROW_TIME);
             element[index].cStatus = UNSUPERVISED;
             break;
           case P_RELEASE:
             if (element[index].state == PM_THROW_RIGHT) {
-              Pdevice(element[index].device1, HIGH, LOW);
+              Pdevice(element[index].deviceMajor, HIGH, LOW);
               element[index].state = PM_POL_RIGHT_DONE;
               element[index].timer = PM_POL_TIME;
             } else {
-              Pdevice(element[index].device1, LOW, LOW);
+              Pdevice(element[index].deviceMajor, LOW, LOW);
               element[index].state = PM_IDLE;
               //            element[index].cStatus = ;--------------- efter hold skal status skiftes FIXME
             }
             break;
         }
         break;
-      case 40: // Signal two aspect, L-device
-      case 42: // Signal three aspect, L-device
+      case 40: // Signal 2 aspect, 2 L-device
+      case 42: // Signal 3 aspect, 2 L-device
         switch (order) {
           case S_STOP:
-            Ldevice(element[index].device1, HIGH);
-            Ldevice(element[index].device2, LOW);
+            Ldevice(element[index].deviceMajor, HIGH);
+            Ldevice(element[index].deviceMajor + 1, LOW);
             element[index].cStatus = I_STOP;
             break;
           case S_PROCEED:
-            Ldevice(element[index].device1, LOW);
-            Ldevice(element[index].device2, HIGH);
+            Ldevice(element[index].deviceMajor, LOW);
+            Ldevice(element[index].deviceMajor + 1, HIGH);
             element[index].timer = CLOSE_TIMER;
             element[index].cStatus = I_PROCEED;
             break;
           case S_PROCEEDPROCEED:
-            Ldevice(element[index].device1, LOW);
-            Ldevice(element[index].device2, flash);
+            Ldevice(element[index].deviceMajor, LOW);
+            Ldevice(element[index].deviceMajor + 1, flash);
             element[index].timer = CLOSE_TIMER;
             element[index].cStatus = I_PROCEEDPROCEED;
             break;
         }
         break;
-      case 41: // Signal, two aspect, U-device
+      case 44: // Signal 3 aspect, 3 L-device
         switch (order) {
           case S_STOP:
-            Udevice(element[index].device1, LOW);
+            Ldevice(element[index].deviceMajor, HIGH);
+            Ldevice(element[index].deviceMajor + 1, LOW);
+            Ldevice(element[index].deviceMajor + 2, LOW);
             element[index].cStatus = I_STOP;
             break;
           case S_PROCEED:
-            Udevice(element[index].device1, HIGH);
+            Ldevice(element[index].deviceMajor, LOW);
+            Ldevice(element[index].deviceMajor + 1, HIGH);
+            Ldevice(element[index].deviceMajor + 2, LOW);
+            element[index].timer = CLOSE_TIMER;
+            element[index].cStatus = I_PROCEED;
+            break;
+          case S_PROCEEDPROCEED:
+            Ldevice(element[index].deviceMajor, LOW);
+            Ldevice(element[index].deviceMajor + 1, HIGH);
+            Ldevice(element[index].deviceMajor + 2, HIGH);
+            element[index].timer = CLOSE_TIMER;
+            element[index].cStatus = I_PROCEEDPROCEED;
+            break;
+        }
+        break;
+      case 41: // Signal, 2 aspect, 1 U-device
+        switch (order) {
+          case S_STOP:
+            Udevice(element[index].deviceMajor, LOW);
+            element[index].cStatus = I_STOP;
+            break;
+          case S_PROCEED:
+            Udevice(element[index].deviceMajor, HIGH);
             element[index].timer = CLOSE_TIMER;
             element[index].cStatus = I_PROCEED;
             break;
         }
         break;
-      case 43: // Signal, three aspect, U-device
+      case 43: // Signal, 3 aspect, 2 U-device
         switch (order) {
           case S_STOP:
-            Udevice(element[index].device1, HIGH);
-            Udevice(element[index].device2, LOW);
+            Udevice(element[index].deviceMajor, HIGH);
+            Udevice(element[index].deviceMajor + 1, LOW);
             element[index].cStatus = I_STOP;
             break;
           case S_PROCEED:
-            Udevice(element[index].device1, LOW);
-            Udevice(element[index].device2, HIGH);
+            Udevice(element[index].deviceMajor, LOW);
+            Udevice(element[index].deviceMajor + 1, HIGH);
             element[index].timer = CLOSE_TIMER;
             element[index].cStatus = I_PROCEED;
             break;
           case S_PROCEEDPROCEED:
-            Udevice(element[index].device1, LOW);
-            Udevice(element[index].device2, flash);
+            Udevice(element[index].deviceMajor, LOW);
+            Udevice(element[index].deviceMajor + 1, flash);
+            element[index].timer = CLOSE_TIMER;
+            element[index].cStatus = I_PROCEEDPROCEED;
+            break;
+        }
+        break;
+      case 45: // Signal, 3 aspect, 3 U-device
+        switch (order) {
+          case S_STOP:
+            Udevice(element[index].deviceMajor, HIGH);
+            Udevice(element[index].deviceMajor + 1, LOW);
+            Udevice(element[index].deviceMajor + 2, LOW);
+            element[index].cStatus = I_STOP;
+            break;
+          case S_PROCEED:
+            Udevice(element[index].deviceMajor, LOW);
+            Udevice(element[index].deviceMajor + 1, HIGH);
+            Udevice(element[index].deviceMajor + 2, LOW);
+            element[index].timer = CLOSE_TIMER;
+            element[index].cStatus = I_PROCEED;
+            break;
+          case S_PROCEEDPROCEED:
+            Udevice(element[index].deviceMajor, LOW);
+            Udevice(element[index].deviceMajor + 1, HIGH);
+            Udevice(element[index].deviceMajor + 2, HIGH);
             element[index].timer = CLOSE_TIMER;
             element[index].cStatus = I_PROCEEDPROCEED;
             break;
@@ -481,22 +589,22 @@ void elementOrder(byte index, byte order) {
       case 30: // Road signal, L-device
         switch (order) {
           case R_PASS:
-            Ldevice(element[index].device1, LOW);
+            Ldevice(element[index].deviceMajor, LOW);
             element[index].cStatus = I_PASS;
             break;
           case R_STOP:
-            Ldevice(element[index].device1, flash);
+            Ldevice(element[index].deviceMajor, flash);
             element[index].cStatus = I_STOP;
             break;
         }
       case 31: // Road signal, U-device
         switch (order) {
           case R_PASS:
-            Udevice(element[index].device1, LOW);
+            Udevice(element[index].deviceMajor, LOW);
             element[index].cStatus = I_PASS;
             break;
           case R_STOP:
-            Udevice(element[index].device1, flash);
+            Udevice(element[index].deviceMajor, flash);
             element[index].cStatus = I_STOP;
             break;
         }
@@ -504,12 +612,12 @@ void elementOrder(byte index, byte order) {
       case 32: // Road barrier, P-device
         switch (order) {
           case L_ACTIVATE:
-            Pdevice(element[index].device1, LOW, HIGH);
+            Pdevice(element[index].deviceMajor, LOW, HIGH);
             element[index].cStatus = I_MOVING_DOWN;
             element[index].timer = LX_MOVING;
             break;
           case L_DEACTIVATE:
-            Pdevice(element[index].device1, HIGH, LOW);
+            Pdevice(element[index].deviceMajor, HIGH, LOW);
             element[index].cStatus = I_MOVING_UP;
             element[index].timer = LX_MOVING;
             break;
