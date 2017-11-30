65c3e5c6d1a703413141688b9fa3a1ddc89c503e
Jan Bertelsen <jb-git@w57.dk>
2017-11-30 19:54:12 +0100

    Small update of PT1 and Train data

--- WaySide/SiteData/CBU/TrainData.php
+++ WaySide/SiteData/CBU/TrainData.php
@@ -4,15 +4,15 @@ $trainData = [
 0 => [
   "ID" => 20,
   "name" => "Cargo",
-  "lengthFront" => 20,
-  "lengthBehind" => 15,
+  "lengthFront" => 40,
+  "lengthBehind" => 55,
   "wheelFactor" => 5,
   ],
 1 => [
   "ID" => 22,
   "name" => "Circus",
-  "lengthFront" => 20,
-  "lengthBehind" => 15,
+  "lengthFront" => 40,
+  "lengthBehind" => 45,
   "wheelFactor" => 5,
   ],
 ];
